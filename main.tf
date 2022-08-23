03ce1157403944975d3c57fd105f07b05909c86a
Job King <jobezking@gmail.com>
2022-08-22 20:10:39 -0700

    count

--- main.tf
+++ main.tf
@@ -13,9 +13,24 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("/mnt/chromeos/removable/expand/terraform-auth/terraform-gcp-77.json")
-
+  #credentials = file("/mnt/chromeos/removable/expand/terraform-auth/terraform-gcp-77.json")
+  credentials = file("/home/jeking/Desktop/terraform-auth/terraform-gcp-77.json")
   project = "terraform-gcp-77"
   region  = "us-central1"
   zone    = "us-central1-f"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  count = 3
+  name         = "terraform-instance${count.index}"
+  machine_type = "t2a-standard-1"
+  zone         = "us-central1-f"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11-bullseye-arm64-v20220719"
+    }
+  }
+  network_interface {
+    network = "default"
+  }
 }
