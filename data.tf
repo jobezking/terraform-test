48b674a2589681dfc9b2667eb512a7d3e4796c3d
Job King <jobezking@gmail.com>
2022-08-23 20:30:09 -0400

    switch from compute engine to storage

--- data.tf
+++ data.tf
@@ -1,16 +1,16 @@
-data "google_compute_instance" "example"{
-    name="gcelab2"
-}
+#data "google_compute_instance" "example"{/store/ginya-izakaya-atlanta-47702/
+#    name="gcelab2"/store/ginya-izakaya-atlant/store/ginya-izakaya-atlanta-47702/a-47702//store/ginya-izakaya-atlanta-47702/
+#}/store/ginya-izakaya-atlanta-47702//store/ginya-izakaya-atlanta-47702/
 
-output "zone" {
-    value = data.google_compute_instance.example.zone
-}
+#output "zone" {
+#    value = data.google_compute_instance.example.zone
+#}
 
-output "vm_name" {
-    value = google_compute_instance.named_instance[*].name
-}
+#output "vm_name" {
+#    value = google_compute_instance.named_instance[*].name
+#}
 
-output "each_vm_name" {
-    value = google_compute_instance.each_instance
-    sensitive = true
-}
+#output "each_vm_name" {
+#    value = google_compute_instance.each_instance
+#    sensitive = true
+#}
--- main.tf
+++ main.tf
@@ -20,62 +20,81 @@ provider "google" {
   zone    = "us-central1-f"
 }
 
-variable "vm_names"{
+variable "bucket_names"{
   description = "list of the virtual machine names"
   type = list(string)
-  default = ["vm1", "vm2", "vm3", "vm4", "vm5"]
+  default = ["terraform-gcp-77-bucket-0", "terraform-gcp-77-bucket-1", 
+              "terraform-gcp-77-bucket-2", "terraform-gcp-77-bucket-3", "terraform-gcp-77-bucket-4"]
 }
 
-variable "more_vm_names"{
-  description = "list of the network interface names"
-  type = list(string)
-  default = ["vm6", "vm7", "vm8", "vm9", "vm0"]
+resource "google_storage_bucket" "count_index" {
+  count = length(var.bucket_names)
+  name          = var.bucket_names[count.index]
+  location      = "US"
 }
 
-resource "google_compute_instance" "vm_instance" {
+resource "google_storage_bucket" "count" {
   count = 3
-  name         = "terraform-instance${count.index}"
-  machine_type = "t2a-standard-1"
-  zone         = "us-central1-f"
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11-bullseye-arm64-v20220719"
-    }
-  }
-  network_interface {
-    network = "default"
-  }
+  name          = "terraform-gcp-77-bucket${count.index}"
+  location      = "US"
 }
 
-resource "google_compute_instance" "named_instance" {
-  count = length(var.vm_names)
-  name         = var.vm_names[count.index]
-  machine_type = "f1-micro"
-  zone         = "us-central1-f"
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-  network_interface {
-    network = "default"
-  }
-}
+#variable "vm_names"{
+#  description = "list of the virtual machine names"
+#  type = list(string)
+#  default = ["vm1", "vm2", "vm3", "vm4", "vm5"]
+#}
 
-resource "google_compute_instance" "each_instance" {
-  for_each = toset(var.more_vm_names)
-  name         = each.value
-  machine_type = "f1-micro"
-  zone         = "us-central1-f"
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-  network_interface {
+#variable "more_vm_names"{
+#  description = "list of the network interface names"
+#  type = list(string)
+#  default = ["vm6", "vm7", "vm8", "vm9", "vm0"]
+#}
+
+#resource "google_compute_instance" "vm_instance" {
+#  count = 3
+#  name         = "terraform-instance${count.index}"
+#  machine_type = "t2a-standard-1"
+#  zone         = "us-central1-f"
+#  boot_disk {
+#    initialize_params {
+#      image = "debian-cloud/debian-11-bullseye-arm64-v20220719"
+#    }
+#  }
+#  network_interface {
+#    network = "default"
+#  }
+#}
+
+#resource "google_compute_instance" "named_instance" {
+#  count = length(var.vm_names)
+#  name         = var.vm_names[count.index]
+#  machine_type = "f1-micro"
+#  zone         = "us-central1-f"
+#  boot_disk {
+#    initialize_params {
+#      image = "debian-cloud/debian-11"
+#    }
+#  }
+#  network_interface {
+#    network = "default"
+#  }
+#}
+
+#resource "google_compute_instance" "each_instance" {
+#  for_each = toset(var.more_vm_names)
+#  name         = each.value
+#  machine_type = "f1-micro"
+#  zone         = "us-central1-f"
+#  boot_disk {
+#    initialize_params {
+#      image = "debian-cloud/debian-11"
+#    }
+#  }
+#  network_interface {
     #for_each = toset(var.net_names)
     #name         = each.value
     #name = google_compute_instance.each_instance[each.key].name.terraform-nic
-    network = "default"
-  }
-}
+#   network = "default"
+#  }
+#}
